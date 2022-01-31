b99d440e28eac513802e52e4212879494e89360e
Frus <mcagdasbayirli@gmail.com>
2022-01-31 16:56:33 +0300

    sorted posts

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import FeaturedPost from "../components/FeaturedPost";
 import Post from "../components/Post";
 import { getPosts } from "../services";
 
-export default function Home({ posts }) {
+export default function Home({ sorted }) {
   return (
     <section className="lg:py-8 sm:py-6 py-4">
       <div className="max-w-7xl mx-auto lg:px-8 sm:px-6 px-4">
@@ -10,11 +10,11 @@ export default function Home({ posts }) {
           Frus Blog
         </h1>
         <main className="relative">
-          <FeaturedPost post={posts.slice(-1)[0].node} />
+          <FeaturedPost post={sorted[0].node} />
           <section className="lg:py-20 sm:py-16 py-10">
             <div className="max-w-7xl mx-auto lg:px-8 sm:px-6 px-0">
               <div className="max-w-md grid grid-cols-1 mx-auto lg:max-w-full md:max-w-2xl md:grid-cols-2 lg:grid-cols-3 lg:gap-x-16 md:gap-x-10 gap-x-5 gap-y-12">
-                {posts.slice(0, -1).map((post, index) => (
+                {sorted.slice(1).map((post, index) => (
                   <Post post={post.node} key={index} />
                 ))}
               </div>
@@ -28,9 +28,10 @@ export default function Home({ posts }) {
 
 export async function getStaticProps() {
   const posts = (await getPosts()) || [];
+  const sorted = posts.reverse();
   return {
     props: {
-      posts,
+      sorted,
     },
     revalidate: 60,
   };
